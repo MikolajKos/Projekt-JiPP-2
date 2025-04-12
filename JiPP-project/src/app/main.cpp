04b68497b6bd4502c5d5827316c07d7e263b6c14
Mikołaj <niko.kosiorek@gmail.com>
2025-04-13 01:12:54 +0200

    Ftxui setup

--- JiPP-project/src/app/main.cpp
+++ JiPP-project/src/app/main.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/component/component.hpp>
-#include <ftxui/dom/elements.hpp>
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
 #include <Student.h>
 #include <SmartArray.h>
+#include <ftxui\component\screen_interactive.hpp>
+#include <ftxui\component\component.hpp>
 
 using namespace ftxui;
 
 int main() {
-    Student student1("Jan", "Kowalski", 20, 12345, 4.5);
-    Student student2("Anna", "Nowak", 22, 67890, 4.8);
+    auto screen = ScreenInteractive::Fullscreen();
+    Component closeButton = Button("Close", screen.ExitLoopClosure());
 
-    SmartArray<Student> studenci;
+    Component renderer = Renderer(closeButton, [&] {
+        return vbox({
+        text("Hello World!"),
+        closeButton->Render()
+            });
+        });
 
-    studenci.pushBack(student1);
-    studenci.pushBack(student2);
-
-    for (unsigned i = 0; i < studenci.size(); ++i) {
-        studenci[i].wypiszInformacje();
-        std::cout << std::endl;
-    }
-
-    studenci.erase(0);
-
-    for (unsigned i = 0; i < studenci.size(); ++i) {
-        studenci[i].wypiszInformacje();
-        std::cout << std::endl;
-    }
-
+    screen.Loop(renderer);
     return 0;
+    
+    //Student student1("Jan", "Kowalski", 20, 12345, 4.5);
+    //Student student2("Anna", "Nowak", 22, 67890, 4.8);
+    //SmartArray<Student> studenci;
+
+    //studenci.pushBack(student1);
+    //studenci.pushBack(student2);
+
+    //for (unsigned i = 0; i < studenci.size(); ++i) {
+    //    studenci[i].wypiszInformacje();
+    //    std::cout << std::endl;
+    //}
+
+    //studenci.erase(0);
+
+    //for (unsigned i = 0; i < studenci.size(); ++i) {
+    //    studenci[i].wypiszInformacje();
+    //    std::cout << std::endl;
+    //}
+
+    //return 0;
 }
